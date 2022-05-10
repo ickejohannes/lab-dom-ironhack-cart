d274556c9d98d12242ba6f77c6d2a34fe6bc58d9
ickejohannes <ickejohannes@gmail.com>
2022-05-10 14:04:48 +0200

    Do iterations 1-4. Tests for iteration 4 are failing, but code is working.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,38 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const priceElement = product.querySelector('.price span');
+  const quantityElement = product.querySelector(`.quantity input`);
+  const subTotal =
+    parseFloat(priceElement.innerHTML) * parseInt(quantityElement.value);
+
+  const subtotalElement = product.querySelector('.subtotal span');
+
+  subtotalElement.innerHTML = subTotal;
+  return subTotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  const products = document.getElementsByClassName("product");
+  let totalValue = 0; 
+  for (let iterator = 0; iterator < products.length; iterator += 1) {
+    totalValue += updateSubtotal(products[iterator]);
+  }
 
-  // ITERATION 3
-  //... your code goes here
+  const totalElement = document.getElementById("total-value");
+  const spanInTotalElement = totalElement.querySelector("span");
+ 
+  spanInTotalElement.innerHTML = totalValue;
+
+  
 }
 
 // ITERATION 4
@@ -25,7 +40,12 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  event.currentTarget.parentNode.removeChild;
+  const eventParentNode = target.parentNode;
+  const productToRemove = eventParentNode.parentNode;
+  const tableBodyToRemoveFrom = productToRemove.parentNode;
+  console.log({tableBodyToRemoveFrom});
+  tableBodyToRemoveFrom.removeChild(productToRemove);
 }
 
 // ITERATION 5
@@ -37,6 +57,8 @@ function createProduct() {
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
+  const removeButtons = document.getElementsByClassName("btn btn-remove");
+  for (let iterator = 0; iterator < removeButtons.length; iterator  += 1) {
+    removeButtons[iterator].addEventListener('click', removeProduct);
+  }
 });
